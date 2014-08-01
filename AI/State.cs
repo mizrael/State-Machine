da3e54b26dcddba307505df18c9f046b2f11d4ae
mizrael <davide.guida@razorfishhealthware.com>
2014-08-01 15:10:24 +0200

    initial commit

--- AI/State.cs
+++ AI/State.cs
@@ -70,21 +70,4 @@ namespace AI
         #endregion Properties
     }
 
-    public class StateTransition
-    {
-        private Func<bool> _condition;
-
-        public StateTransition(State to, Func<bool> condition)
-        {
-            To = to;
-            _condition = condition;
-        }
-
-        public bool Check()
-        {
-            return (_condition());
-        }
-
-        public State To { get; private set; }
-    }
 }
---
+++ AI/StateTransition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI
+{ 
+    public class StateTransition
+    {
+        private Func<bool> _condition;
+
+        public StateTransition(State to, Func<bool> condition)
+        {
+            To = to;
+            _condition = condition;
+        }
+
+        public bool Check()
+        {
+            return (_condition());
+        }
+
+        public State To { get; private set; }
+    }
+}
